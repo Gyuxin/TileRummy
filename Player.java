84561cc9a3c9c23883901ceb5098860557df0939
GaoYuxin <gaomnemosyne@Gmail.com>
2018-10-01 14:32:50 -0400

    add sort() method in Player class

--- Player.java
+++ Player.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;  
+import java.util.Comparator;
 
 public class Player {
 	
@@ -9,8 +11,10 @@ public class Player {
 		
 	}
 	
+	//Shuffle user's hand tile
 	public void sort() {
-		
+		Comparator<Tile> comp = new CompareTile();
+		Collections.sort(myHandTile, comp);	
 	}
 	
 	public int getNumberOfHandTile() {
@@ -55,3 +59,43 @@ public class Player {
 	}
 	
 }
+
+/* compare each tile in user's hand 
+ * in order to sort them in order
+ * R B G D
+ * */
+class CompareTile implements Comparator<Tile>{
+	
+	@Override
+	public int compare(Tile t1, Tile t2) {
+		int i = covertColorToInteger(t1).compareTo(covertColorToInteger(t2));
+		if (i != 0) return i;
+		
+		Integer r1 = new Integer(t1.getRank());
+		Integer r2 = new Integer(t2.getRank());
+		i = r1.compareTo(r2);
+		return i;
+	}
+	
+	
+	/* Convert tile's color to Integer
+	 * So that it can be compared through .compareTo()
+	 * R < B < G < D
+	 * */
+	public Integer convertColorToInteger(Tile t) {
+		if(t.getColor().equals('R')) {
+			Integer i = new Integer(1);
+			return i;
+		} else if(t.getColor().equals('B')) {
+			Integer i = new Integer(2);
+			return i;
+		} else if(t.getColor().equals('G')) {
+			Integer i = new Integer(3);
+			return i;
+		} else {
+			Integer i = new Integer(4);
+			return i;
+		}
+	}
+}
+
