161056b94e48a9335cc6b2877dfd9172a016264e
VINCENT\Vince <vincentwang333@outlook.com>
2018-10-01 12:38:23 -0400

    computer class base on player class version1

---
+++ Computer.java
@@ -0,0 +1,32 @@
+class Computer extends Player
+{
+  // Stratergy;
+
+  public computer()
+  {
+    super();
+  }
+  public int getNumberOfHandCard()
+  {
+    super.getNumberOfHandCard();
+  }
+  public void initialHandCard(Deck d)
+  {
+    super.initialCard(d);
+  }
+  public void drawATile(Tile t)
+  {
+    super.drawATile(t);
+  }
+
+  public void dealCard()
+  {
+
+  }
+  //by using stratergy
+  // public void dealCardAccordingStratergy()
+  // {
+  //
+  // }
+
+}
