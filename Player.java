36f58becc0392ed7609f67b9c78bcf3459ae7804
GaoYuxin <gaomnemosyne@Gmail.com>
2018-10-01 12:31:51 -0400

    create more methods in Player class

--- Player.java
+++ Player.java
@@ -3,24 +3,55 @@ import java.util.List;
 
 public class Player {
 	
-	private List<Tile> myHandCard = new ArrayList<Tile>();
+	private List<Tile> myHandTile = new ArrayList<Tile>();
 	
 	public Player() {
 		
 	}
 	
-	public int getNumberOfHandCard() {
-		return this.myHandCard.size();
+	public void sort() {
+		
 	}
 	
-	public void initialHandCard(Deck d){
+	public int getNumberOfHandTile() {
+		return this.myHandTile.size();
+	}
+	
+	public void initialHandTile(Deck d){
 		for(int i = 0; i < 14; i++) {
-			myHandCard.add(d.drawTile());
+			myHandTile.add(d.drawTile());
 		}
 	}
 	
+	public boolean hasMeld() {
+		
+	}
+	
+	public ArrayList<Tile> findMeld() {
+		
+	}
+	
+	public boolean initialCheck(ArrayList<Tile> meld) {
+		
+	}
+	
+	public ArrayList<Tile> dealTile(ArrayList<Tile> al){
+		myHandTile.remove(al);
+	}
+	
+	
 	public Tile drawATile(Tile t) {
-		myHandCard.add(t);
+		myHandTile.add(t);
+		return t;
+	}
+	
+	public String toString() {
+		String result = "Hand Tile: ";
+		for(int i = 0; i < this.myHandTile.size(); i++) {
+			result+= this.myHandTile.get(i).getColor()+
+					this.myHandTile.get(i).getRank()+" ";
+		}
+		return result;
 	}
 	
 }
