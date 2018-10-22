5fb3582f41d50ac3390624782e7970de21a526f1
‘gracexyz’ <xinyizhong@Graces-MacBook-Pro-2.local>
2018-10-22 14:50:25 -0400

    some github errors= =

---
+++ src/main/java/tilerummy/Tile.java
@@ -0,0 +1,60 @@
+package tilerummy;
+
+public class Tile {
+	
+	private String color;
+	private int number;
+	private boolean justPlayed;
+	private boolean justMoved;
+	
+	public Tile(String color, int number) {
+		this.color = color;
+		this.number = number;
+		this.justPlayed = false;
+		this.justMoved = false;
+	}
+
+	public Tile(String name) {
+		this.color = name.substring(0,1);
+		this.number = Integer.parseInt(name.substring(1));
+		this.justPlayed = false;
+		this.justMoved = false;
+	}
+	
+	public void printTile() {
+		System.out.print(this.color + this.number + " ");
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+
+	public boolean getJustPlayed(){
+		return this.justPlayed;
+	}
+
+	public void setJustPlayed(){
+		if(getJustPlayed()){
+			justPlayed = false;
+		} else {
+			justPlayed = true;
+		}
+	}
+
+	public boolean getJustMoved(){
+		return this.justMoved;
+	}
+
+	public void setJustMoved(){
+		if(getJustMoved()){
+			justMoved = false;
+		} else {
+			justMoved = true;
+		}
+	}
+
+}
