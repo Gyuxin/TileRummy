be2b617863cd9af603dec3ae3de9a31e72c093d5
GaoYuxin <gaomnemosyne@Gmail.com>
2018-10-23 00:22:45 -0400

    test: add a test case for player

--- src/test/java/tilerummy/PlayerTest.java
+++ src/test/java/tilerummy/PlayerTest.java
@@ -41,6 +41,47 @@ public class PlayerTest extends TestCase {
 //	      
 	      //p.hasSet();
 	      
+//	      System.out.println(p.toString());
+//	      p.hasMeld();
+//	      System.out.println("begin");
+////	      assertEquals(9, p.getNumberOfHandTile());
+//	      for(int i = 0; i< p.getMyMeld().size();i++) {
+//	    	  for(int j = 0; j<p.getMyMeld().get(i).size(); j++){
+//	    	      p.getMyMeld().get(i).get(j).printTile();
+//	    	  }
+//	    	  System.out.println("");
+//	      }
+//	      System.out.println("end");
+	}
+	
+	public void testCase2() {
+		Player p = new Player();
+		  Tile t1 = new Tile("R",3);
+	      Tile t2 = new Tile("R",4);
+	      Tile t3 = new Tile("R",5);
+	      Tile t4 = new Tile("B",4);
+	      Tile t5 = new Tile("B",5);
+	      Tile t6 = new Tile("B",6);
+	      Tile t7 = new Tile("G",5);
+	      Tile t8 = new Tile("R",8);
+	      Tile t9 = new Tile("B",8);
+	      Tile t10 = new Tile("O",8);
+
+	      
+	      p.drawATile(t1);
+	      p.drawATile(t2);
+	      p.drawATile(t3);
+	      p.drawATile(t4);
+	      p.drawATile(t5);
+	      p.drawATile(t6);
+	      p.drawATile(t7);
+	      p.drawATile(t8);
+	      p.drawATile(t9);
+	      p.drawATile(t10);
+
+//	      
+	      //p.hasSet();
+	      
 	      System.out.println(p.toString());
 	      p.hasMeld();
 	      System.out.println("begin");
