d7731b2911a70231069a7f77a193b37aff282f80
GaoYuxin <gaomnemosyne@Gmail.com>
2018-10-22 23:27:17 -0400

    Test: add a new test case to test player

--- src/test/java/tilerummy/PlayerTest.java
+++ src/test/java/tilerummy/PlayerTest.java
@@ -8,34 +8,41 @@ public class PlayerTest extends TestCase {
 	public void testHasSet() {
 		Player p = new Player();
 		  Tile t1 = new Tile("R",8);
-	      Tile t2 = new Tile("R",9);
-	      Tile t3 = new Tile("R",10);
-	      Tile t4 = new Tile("B",10);
-	      Tile t5 = new Tile("O",10);
-//	      Tile t6 = new Tile("R",1);
-//	      Tile t7 = new Tile("R",4);
-//	      Tile t8 = new Tile("R",3);
-//	      Tile t9 = new Tile("G",1);
+	      Tile t2 = new Tile("R",10);
+	      Tile t3 = new Tile("R",11);
+	      Tile t4 = new Tile("R",12);
+	      Tile t5 = new Tile("B",1);
+	      Tile t6 = new Tile("B",10);
+	      Tile t7 = new Tile("B",11);
+	      Tile t8 = new Tile("G",1);
+	      Tile t9 = new Tile("G",4);
+	      Tile t10 = new Tile("G",8);
+	      Tile t11 = new Tile("G",10);
+	      Tile t12 = new Tile("O",3);
+	      Tile t13 = new Tile("O",8);
+	      Tile t14 = new Tile("O",11);
+	      Tile t15 = new Tile("O",13);
 	      
 	      p.drawATile(t1);
 	      p.drawATile(t2);
 	      p.drawATile(t3);
 	      p.drawATile(t4);
 	      p.drawATile(t5);
-//	      p.drawATile(t6);
-//	      p.drawATile(t7);
-//	      p.drawATile(t8);
-//	      p.drawATile(t9);
+	      p.drawATile(t6);
+	      p.drawATile(t7);
+	      p.drawATile(t8);
+	      p.drawATile(t9);
+	      p.drawATile(t10);
+	      p.drawATile(t11);
+	      p.drawATile(t12);
+	      p.drawATile(t13);
+	      p.drawATile(t14);
+	      p.drawATile(t15);
 //	      
 	      //p.hasSet();
-	      p.sort();
-	      for(int i = 0; i<p.getMyHandTile().size(); i++) {
-	    	  p.getMyHandTile().get(i).printTile();
-	      }
+	      
 	      System.out.println(p.toString());
 	      p.hasMeld();
-	      System.out.println(p.toString());
-	      p.hasRun();  
 	      System.out.println("begin");
 //	      assertEquals(9, p.getNumberOfHandTile());
 	      for(int i = 0; i< p.getMyMeld().size();i++) {
