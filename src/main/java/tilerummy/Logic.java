7984df1aca67417dc6b77d6df6550abd78a95d71
‘gracexyz’ <xinyizhong@Graces-MacBook-Pro-2.local>
2018-10-23 00:13:24 -0400

    refactory

--- src/main/java/tilerummy/Logic.java
+++ src/main/java/tilerummy/Logic.java
@@ -2,7 +2,7 @@ package tilerummy;
 
 public class Logic {
 
-    public static void addOneTile(Tile t, Table table){
+    public static boolean addOneTile(Tile t, Table table){
 
         for(int i = 0; i < table.getSize(); i++){
 
@@ -12,21 +12,26 @@ public class Logic {
             if(currentMeld.isSet()){
                 if(!currentMeld.containsTile(t)){
                     currentMeld.addTileAtLast(t);
+                    return true;
                 }
             } else {
                 //If tile's number is one less than first tile's number in current meld
                 if(t.getNumber()+1 == currentMeld.getFirstTile().getNumber()){
                     currentMeld.addTileAtFirst(t);
+                    return true;
                 } 
 
                 //If tile's number is one bigger than last tile's number in current meld
                 if(t.getNumber()-1 == currentMeld.getLastTile().getNumber()){
                     currentMeld.addTileAtLast(t);
+                    return true;
                 } 
             }
         }
 
         table.removeEmptyMeld();
+
+        return false;
 
     }
 
